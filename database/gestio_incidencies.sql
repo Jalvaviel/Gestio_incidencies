8ef61ce73b21b0afb4ea1c0d4da507c25eae4e01
Jalvaviel <jalvaviel@gmail.com>
2023-11-03 08:11:17 +0100

    Ampliada la base de dades

--- database/gestio_incidencies.sql
+++ database/gestio_incidencies.sql
@@ -1,43 +1,105 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
 --
--- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 02-11-2023 a las 11:33:03
--- Versión del servidor: 8.0.31
--- Versión de PHP: 8.0.26
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: 127.0.0.1    Database: gestio_incidencies
+-- ------------------------------------------------------
+-- Server version	8.0.31
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Base de datos: `gestio_incidencies`
+-- Table structure for table `devices`
 --
 
--- --------------------------------------------------------
+DROP TABLE IF EXISTS `devices`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `devices` (
+  `id_device` int NOT NULL,
+  `os` varchar(200) COLLATE utf8mb4_spanish_ci DEFAULT NULL COMMENT 'Operative System',
+  `code` varchar(200) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `description` varchar(2000) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `room` int DEFAULT NULL,
+  `ip` varchar(15) COLLATE utf8mb4_spanish_ci DEFAULT NULL COMMENT 'internet protocol v4',
+  PRIMARY KEY (`id_device`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estructura de tabla para la tabla `users`
+-- Dumping data for table `devices`
+--
+
+LOCK TABLES `devices` WRITE;
+/*!40000 ALTER TABLE `devices` DISABLE KEYS */;
+/*!40000 ALTER TABLE `devices` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `incidents`
+--
+
+DROP TABLE IF EXISTS `incidents`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `incidents` (
+  `id_incident` int NOT NULL,
+  `description` varchar(2000) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `status` varchar(20) COLLATE utf8mb4_spanish_ci DEFAULT NULL COMMENT 'resolved/unresolved',
+  `date` date DEFAULT NULL,
+  PRIMARY KEY (`id_incident`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `incidents`
+--
+
+LOCK TABLES `incidents` WRITE;
+/*!40000 ALTER TABLE `incidents` DISABLE KEYS */;
+/*!40000 ALTER TABLE `incidents` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
 --
 
 DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `ID` int NOT NULL,
-  `NAME` varchar(200) COLLATE utf8mb4_spanish_ci NOT NULL,
-  `SURNAME` varchar(200) COLLATE utf8mb4_spanish_ci NOT NULL,
-  `EMAIL` varchar(200) COLLATE utf8mb4_spanish_ci NOT NULL,
-  `PASSWORD` varchar(256) COLLATE utf8mb4_spanish_ci NOT NULL,
-  PRIMARY KEY (`ID`)
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id_user` int NOT NULL,
+  `name` varchar(200) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `surname` varchar(200) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `email` varchar(200) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `password` varchar(256) COLLATE utf8mb4_spanish_ci NOT NULL,
+  PRIMARY KEY (`id_user`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
-COMMIT;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-11-02 13:24:59
