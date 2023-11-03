88a43e70f315e10be0b27f2a348dc04c52dc4c20
Gerard Puig Ortega <gerard.puig.ortega@hotmail.com>
2023-11-03 12:24:31 +0100

    He modificado algunas cosillas

--- library/administrador.php
+++ library/administrador.php
@@ -3,17 +3,23 @@ include "helpers.php";
 include "treballador.php";
 include "tecnic.php";
 
-function insert_into_users($conexio, $id, $name, $surname, $email, $password, $role)
+function insert_into_users($id, $name, $surname, $email, $password, $role)
 {
+    $env = parse_ini_file('.env');
+    $db_user = $env['admin'];
+    $db_password = $env['admin_password'];
     $enc_passwd = hash("sha256", $password);
-    $connect = database_connect();
+    $connect = database_connect($db_user, $db_password);
     mysqli_query($connect, "insert $id, $name, $surname, $email, $enc_passwd, $role into gestio_incidencies.users");
     mysqli_close($connect);
 }
 
-function insert_into_devices($conexio, $id,$os, $code, $description, $room, $ip)
+function insert_into_devices($id,$os, $code, $description, $room, $ip)
 {
-    $connect = database_connect();
+    $env = parse_ini_file('.env');
+    $db_user = $env['admin'];
+    $db_password = $env['admin_password'];
+    $connect = database_connect($db_user, $db_password);
     mysqli_query($connect, "insert $id, $os, $code, $description, $room, $ip into gestio_incidencies.incidents");
     mysqli_close($connect);
 }
--- library/helpers.php
+++ library/helpers.php
@@ -1,22 +1,23 @@
 <?php
 function send_to($url)  // Función que sirve para redireccionar a otra página
-{    header("Location: $url");
+{    
+    header("Location: $url");
     die();
 }
 
-function database_connect() // Función que crea la conexion con la base de datos gestio_incidencies.
+function database_connect($user, $password) // Función que crea la conexion con la base de datos gestio_incidencies.
 {
-    $env = parse_ini_file('.env');
-    $user = $env['user'];
-    $password = $env['password'];
     return mysqli_connect("127.0.0.1", $user, hash_passwords($password), "gestio_incidencies");
 }
 
 function test_database_connection()
 {
-    $connect = database_connect(); // Función que hace una prueba para saber si hace bien la conexión y debugging.
+    $env = parse_ini_file('.env');
+    $db_user = $env['admin'];
+    $db_password = $env['admin_password'];
+    $connect = database_connect($db_user, $db_password); // Función que hace una prueba para saber si hace bien la conexión y debugging.
     if (!$connect) 
-    {
+    { 
         echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
         echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
         echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
