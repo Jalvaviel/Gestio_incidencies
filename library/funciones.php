ecb3ce8e956b5215858f55dd62de447ab7fef369
Gerard Puig Ortega <gerard.puig.ortega@hotmail.com>
2023-11-03 09:19:24 +0100

    He traducido cosas al català

--- library/funciones.php
+++ library/funciones.php
@@ -1,27 +1,27 @@
 <?php
-function send_to($url)  // Función que sirve para redireccionar a otra página
+function send_to($url)  // Funció que serveix per redireccionar a una altra pàgina.
 {    header("Location: $url");
     die();
 }
 
-function database_connect() // Función que crea la conexion con la base de datos gestio_incidencies.
+function database_connect()  // Funció que crea la connexió amb la base de dades gestio_incidencies.
 {
     return mysqli_connect("127.0.0.1", "jalvabot", "Xf4,5iB8£9q3%", "gestio_incidencies");
 }
 
-function test_database_connexion()
+function test_database_connexion()  // Funció que fa una prova per saber si fa bé la connexió i debugging.
 {
-    $connect = database_connect(); // Función que hace una prueba para saber si hace bien la conexión y debugging.
+    $connect = database_connect(); 
     if (!$connect) 
     {
-        echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
-        echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
-        echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
+        echo "Error: No s'ha pogut connectar a MySQL." . PHP_EOL;
+        echo "error de depuració: " . mysqli_connect_errno() . PHP_EOL;
+        echo "error de depuració: " . mysqli_connect_error() . PHP_EOL;
         exit;
     }
 
-    echo "Éxito: Se realizó una conexión apropiada a MySQL! La base de datos mi_bd es genial." . PHP_EOL;
-    echo "Información del host: " . mysqli_get_host_info($connect) . PHP_EOL;
+    echo "Èxit: S'ha establert una connexió correcta a MySQL! La base de dades a gestio_incidencies és excelent." . PHP_EOL;
+    echo "Informació del host: " . mysqli_get_host_info($connect) . PHP_EOL;
 
     mysqli_close($connect);
 }
