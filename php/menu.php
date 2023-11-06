b9e24121e6fac4f1faecde194d239f0fee1c1861
Gerard Puig Ortega <gerard.puig.ortega@hotmail.com>
2023-11-06 17:25:27 +0100

    He puesto ya los mecanismos en el menu.php para que dependiendo el rol, tengas unas opciones o otras.

--- php/menu.php
+++ php/menu.php
@@ -16,10 +16,44 @@
                     
             ?>
                     <div>
-                        <a href="">Incidencies</a>
+                        <a href="">Mostrar incidencies</a>
                     </div>
                     <div>
                         <a href="">Reportar incidència</a>
+                    </div>
+            <?php
+                }
+                if(strcmp($rol,"tecnic") == 0 || strcmp($rol,"admin") == 0)
+                {
+            ?>
+                    <div>
+                        <a href="">Modificar incidències</a>
+                    </div>
+                    <div>
+                        <a href="">Borrar incidències</a>
+                    </div>
+            <?php
+                }
+                if(strcmp($rol,"admin") == 0)
+                {
+            ?>
+                    <div>
+                        <a href="">Crear usuari</a>
+                    </div>
+                    <div>
+                        <a href="">Modificar usuari</a>
+                    </div>
+                    <div>
+                        <a href="">Borrar Usuari</a>
+                    </div>
+                    <div>
+                        <a href="">Crear dispositius</a>
+                    </div>
+                    <div>
+                        <a href="">Modificar dispositius</a>
+                    </div>
+                    <div>
+                        <a href="">Borrar dispositius</a>
                     </div>
             <?php
                 }
