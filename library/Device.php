9d5c4ffb805e3897c01c717a1a5c98754137f946
Jalvaviel <jalvaviel@gmail.com>
2023-11-07 13:14:26 +0100

    He actualitzat Device.php amb els camps de ID

--- library/Device.php
+++ library/Device.php
@@ -1,6 +1,6 @@
 <?php
 
-class Device
+#[AllowDynamicProperties] class Device // This allows the properties in the builder to change dinamically, since it is called again in "select" function
 {
     private string $os;
     private string $code;
@@ -8,8 +8,9 @@ class Device
     private string $ip;
     private string $room;
 
-    public function createDevice($os, $code, $description, $ip, $room) : void
+    public function __construct($id, $os, $code, $description, $ip, $room)
     { // Builder.
+        $this->id = $id;
         $this->os = $os;
         $this->code = $code;
         $this->description = $description;
@@ -20,6 +21,7 @@ class Device
     public function getDeviceProperties() : array
     { // Associative array getter.
         return [
+            'id' => $this->id,
             'os' => $this->os,
             'code' => $this->code,
             'description' => $this->description,
@@ -37,21 +39,26 @@ class Device
         mysqli_close($connect);
     }
 
-    public function selectDeviceFromDatabase(string $type, int $id) : void
+    public function selectDeviceFromDatabase(string $type) : void
     {
         $connect = databaseConnect($type);
         $statement = $connect->prepare("SELECT * FROM gestio_incidencies.devices WHERE id_device = ?"); // Prepare and execute the query to prevent SQL attacks.
-        $statement->execute([$id]);
+        $statement->execute([$this->id]);
         $device = $statement->get_result()->fetch_assoc();
-        $this->createDevice($device['os'],$device['code'],$device['description'],$device['room'],$device['ip']);
+        $this->id = $device['id']; // This is niggerlicious
+        $this->os = $device['os'];
+        $this->code = $device['code'];
+        $this->description = $device['description'];
+        $this->room = $device['room'];
+        $this->ip = $device['ip'];
         mysqli_close($connect);
     }
 
-    public function deleteDeviceFromDatabase(string $type, int $id) : void
+    public function deleteDeviceFromDatabase(string $type) : void
     {
         $connect = databaseConnect($type);
         $statement = $connect->prepare("DELETE FROM gestio_incidencies.devices WHERE id_device = ?"); // Prepare and execute the insert to prevent SQL attacks.
-        $statement->execute([$id]);
+        $statement->execute([$this->id]);
         mysqli_close($connect);
     }
 }
