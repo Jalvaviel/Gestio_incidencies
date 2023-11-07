cf06c38667a78780177f27ab0e3bede95f7f4c49
Gerard Puig Ortega <gerard.puig.ortega@hotmail.com>
2023-11-07 13:10:12 +0100

    He empezado la clase users y hago commit pq no creo que me de tiempo a acabarlo

--- library/Device.php
+++ library/Device.php
@@ -8,7 +8,7 @@ class Device
     private string $ip; // IP del dispositiu.
     private string $room; // Aula del dispositiu.
 
-    public function createDevice($os, $code, $description, $ip, $room) : void
+    public function __construct($os, $code, $description, $ip, $room)
     { // Constructor del device
         $this->os = $os;
         $this->code = $code;
@@ -28,7 +28,7 @@ class Device
         ];
     }
 
-    public function insert(string $type) : void // Funció que fa una consulta a la base de dades, i necesita el "role" per comprovar permisos
+    public function insert(string $type) : void // Metod que fa una consulta a la base de dades, i necesita el "role" per comprovar permisos
     {
         $connect = databaseConnect($type); // Crida la funcio databaseConnect, que inicia amb el usuari equivalent als permisos del usuari, i af un mysqli_connect()
         $statement = $connect->prepare("INSERT INTO gestio_incidencies.devices VALUES (DEFAULT,?,?,?,?,?)"); // Prepara i executa el insert per prevenir SQL injections.
@@ -42,7 +42,7 @@ class Device
         $statement = $connect->prepare("SELECT * FROM gestio_incidencies.devices WHERE id_device = ?"); // Prepara i executa el insert per prevenir SQL injections.
         $statement->execute([$id]);
         $device = $statement->get_result()->fetch_assoc();
-        $this->createDevice($device['os'],$device['code'],$device['description'],$device['room'],$device['ip']);
+        $this->__construct($device['os'],$device['code'],$device['description'],$device['room'],$device['ip']);
         mysqli_close($connect);
     }
 
