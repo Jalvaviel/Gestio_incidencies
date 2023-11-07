88c2bafc67544040985f7349e1b5c484df5eccc7
Gerard Puig Ortega <gerard.puig.ortega@hotmail.com>
2023-11-07 12:51:41 +0100

    He comentado algunas cosas y reducido el nombre de algunos metodos

--- library/Device.php
+++ library/Device.php
@@ -2,14 +2,14 @@
 
 class Device
 {
-    private string $os;
-    private string $code;
-    private string $description;
-    private string $ip;
-    private string $room;
+    private string $os; // Sistema operatiu.
+    private string $code; // Codi únic.
+    private string $description; // Descripció del dispositiu.
+    private string $ip; // IP del dispositiu.
+    private string $room; // Aula del dispositiu.
 
     public function createDevice($os, $code, $description, $ip, $room) : void
-    { // Builder.
+    { // Constructor del device
         $this->os = $os;
         $this->code = $code;
         $this->description = $description;
@@ -17,8 +17,8 @@ class Device
         $this->ip = $ip;
     }
 
-    public function getDeviceProperties() : array
-    { // Associative array getter.
+    public function getProperties() : array
+    { // Getter d'una array associativa
         return [
             'os' => $this->os,
             'code' => $this->code,
@@ -28,29 +28,28 @@ class Device
         ];
     }
 
-
-    public function insertDeviceIntoDatabase(string $type) : void
+    public function insert(string $type) : void // Funció que fa una consulta a la base de dades, i necesita el "role" per comprovar permisos
     {
-        $connect = databaseConnect($type);
-        $statement = $connect->prepare("INSERT INTO gestio_incidencies.devices VALUES (DEFAULT,?,?,?,?,?)"); // Prepare and execute the insert to prevent SQL attacks.
-        $statement->execute([$this->os,$this->code,$this->description,$this->room,$this->ip]);
+        $connect = databaseConnect($type); // Crida la funcio databaseConnect, que inicia amb el usuari equivalent als permisos del usuari, i af un mysqli_connect()
+        $statement = $connect->prepare("INSERT INTO gestio_incidencies.devices VALUES (DEFAULT,?,?,?,?,?)"); // Prepara i executa el insert per prevenir SQL injections.
+        $statement->execute([$this->os,$this->code,$this->description,$this->room,$this->ip]); // Executa la consulta.
         mysqli_close($connect);
     }
 
-    public function selectDeviceFromDatabase(string $type, int $id) : void
+    public function select(string $type, int $id) : void
     {
         $connect = databaseConnect($type);
-        $statement = $connect->prepare("SELECT * FROM gestio_incidencies.devices WHERE id_device = ?"); // Prepare and execute the query to prevent SQL attacks.
+        $statement = $connect->prepare("SELECT * FROM gestio_incidencies.devices WHERE id_device = ?"); // Prepara i executa el insert per prevenir SQL injections.
         $statement->execute([$id]);
         $device = $statement->get_result()->fetch_assoc();
         $this->createDevice($device['os'],$device['code'],$device['description'],$device['room'],$device['ip']);
         mysqli_close($connect);
     }
 
-    public function deleteDeviceFromDatabase(string $type, int $id) : void
+    public function delete(string $type, int $id) : void
     {
         $connect = databaseConnect($type);
-        $statement = $connect->prepare("DELETE FROM gestio_incidencies.devices WHERE id_device = ?"); // Prepare and execute the insert to prevent SQL attacks.
+        $statement = $connect->prepare("DELETE FROM gestio_incidencies.devices WHERE id_device = ?"); // Prepara i executa el insert per prevenir SQL injections.
         $statement->execute([$id]);
         mysqli_close($connect);
     }
