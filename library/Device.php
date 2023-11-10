7fdd6c01689706ff5af6376e41f3fa7799fbaa7a
Jalvaviel <jalvaviel@gmail.com>
2023-11-10 10:03:01 +0100

    He actualitzat Device.php

--- library/Device.php
+++ library/Device.php
@@ -1,27 +1,28 @@
 <?php
+include 'helpers.php';
 
-#[AllowDynamicProperties] class Device // This allows the properties in the builder to change dinamically, since it is called again in "select" function
+class Device // This allows the properties in the builder to change dynamically, since it is called again in "select" function
 {
+    private int $id_device;
     private string $os;
     private string $code;
     private string $description;
     private string $ip;
     private string $room;
 
-    public function __construct($id, $os, $code, $description, $ip, $room)
+    public function __construct($id_device, $os, $code, $description, $ip, $room)
     { // Builder.
-        $this->id = $id;
+        $this->id_device = $id_device;
         $this->os = $os;
         $this->code = $code;
         $this->description = $description;
         $this->room = $room;
         $this->ip = $ip;
     }
-
     public function getDeviceProperties() : array
     { // Associative array getter.
         return [
-            'id' => $this->id,
+            'id_device' => $this->id_device,
             'os' => $this->os,
             'code' => $this->code,
             'description' => $this->description,
@@ -30,12 +31,18 @@
         ];
     }
 
-
     public function insertDeviceIntoDatabase(string $type) : void
     {
         $connect = databaseConnect($type);
-        $statement = $connect->prepare("INSERT INTO gestio_incidencies.devices VALUES (DEFAULT,?,?,?,?,?)"); // Prepare and execute the insert to prevent SQL attacks.
-        $statement->execute([$this->os,$this->code,$this->description,$this->room,$this->ip]);
+        $statement = $connect->prepare("INSERT INTO gestio_incidencies.devices VALUES (?,?,?,?,?,?)"); // Prepare and execute the insert to prevent SQL attacks.
+        $statement->bind_param('isssis',$this->id_device,$this->os,$this->code,$this->description,$this->room,$this->ip);
+        $statement->execute();
+        if ($statement->execute()) {
+            echo "Data inserted successfully.";
+        } else {
+            echo "Error: " . $connect->error;
+        }
+
         mysqli_close($connect);
     }
 
@@ -43,22 +50,34 @@
     {
         $connect = databaseConnect($type);
         $statement = $connect->prepare("SELECT * FROM gestio_incidencies.devices WHERE id_device = ?"); // Prepare and execute the query to prevent SQL attacks.
-        $statement->execute([$this->id]);
-        $device = $statement->get_result()->fetch_assoc();
-        $this->id = $device['id']; // This is niggerlicious
-        $this->os = $device['os'];
-        $this->code = $device['code'];
-        $this->description = $device['description'];
-        $this->room = $device['room'];
-        $this->ip = $device['ip'];
-        mysqli_close($connect);
+        $statement->bind_param('i',$this->id_device);
+        $statement->execute();
+        if ($statement->execute()) {
+            $device = $statement->get_result()->fetch_assoc();
+            $this->id = $device['id_device']; // This is niggerlicious
+            $this->os = $device['os'];
+            $this->code = $device['code'];
+            $this->description = $device['description'];
+            $this->room = $device['room'];
+            $this->ip = $device['ip'];
+            mysqli_close($connect);
+            echo "Data inserted successfully.";
+        } else {
+            echo "Error: " . $connect->error;
+        }
     }
 
     public function deleteDeviceFromDatabase(string $type) : void
     {
         $connect = databaseConnect($type);
         $statement = $connect->prepare("DELETE FROM gestio_incidencies.devices WHERE id_device = ?"); // Prepare and execute the insert to prevent SQL attacks.
-        $statement->execute([$this->id]);
+        $statement->bind_param('i',$this->id_device);
+        $statement->execute();
+        if ($statement->execute()) {
+            echo "Data inserted successfully.";
+        } else {
+            echo "Error: " . $connect->error;
+        }
         mysqli_close($connect);
     }
 }
