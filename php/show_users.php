f12cd296410ec52a74a5c085889323743bf38cdc
Jalvaviel <jalvaviel@gmail.com>
2023-11-15 08:58:22 +0100

    He modificat la plana que mostra les dades dels equips.

--- php/show_users.php
+++ php/show_users.php
@@ -7,38 +7,51 @@
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans+Condensed&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans+Condensed:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
 </head>
 <body>
 <header>
-    <img id="logo" src="../png/logo-no-background.png" alt="logo" width="300"/>
+    <img id="logo" src="../png/logo-no-background.png" alt="logo" width="200"/>
     <nav id="mainmenu">
         <a href="login.php" class="mainmenu">Usuaris</a>
         <a href="login.php" class="mainmenu">Incidències</a>
         <a href="login.php" class="mainmenu">Equips</a>
     </nav>
     <nav id="mainoptions">
-        <a href="login.php" id="logout"><img src="../png/exit.png" alt="logout" width="51"/>Surt</a>
-        <a href="login.php" id="profile"><img src="../png/user.png" alt="profile" width="51"/>Perfil</a>
+        <a href="login.php" id="profile"><img src="../png/user.png" alt="profile" width="41"/>Perfil</a>
+        <a href="login.php" id="logout"><img src="../png/exit.png" alt="logout" width="41"/>Surt</a>
     </nav>
 </header>
 <nav class="menu">
-    <ul>
+    <table>
+        <tr>
+            <th><strong>ID Usuari</strong></th>
+            <th><strong>Nom</strong></th>
+            <th><strong>Cognom</strong></th>
+            <th><strong>Correu</strong></th>
+            <th><strong>Contrasenya (Hash)</strong></th>
+            <th><strong>Rol</strong></th>
+            <th><strong>Modificar</strong></th>
+        </tr>
         <?php
         include "../library/User.php";
         try {
             $all_users = loadUsers('admin', 1); // TODO change to variable in session from database
             foreach($all_users as $user) {
                 $user_assoc = $user->getProperties();
+                echo "<tr>";
                 foreach($user_assoc as $key => $value){ // TODO don't show ids and role for normal users, or at least don't let them change it.
-                    echo "<li class='llista'>$key => $value</li>";
+                    echo "<td class='llista'>$value</td>";
                 }
+                echo '<td><a href=\'login.php\'><img src=\'../png/setting.png\' alt=\'configura\' width=\'25\'/></a></td>';
+                echo "</tr>";
             }
         }catch (Exception $e) {
             echo "<div class='error'>Error obtenint resultats.</div>";
         }
         ?>
-    </ul>
+    </table>
 </nav>
 <footer>
     Copyright lolololololol - Javier Castelblanque Iñigo - Gerard Puig Ortega.
