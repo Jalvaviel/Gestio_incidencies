0b6c6e5ca2c8b677aedb45273ab2e3e985b1ae40
Jalvaviel <jalvaviel@gmail.com>
2023-11-23 21:11:24 +0100

    Push final?

--- library/test_device.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include "Device.php";
-    $PC02 = new Device(1,'Windows 11','324AC','PC Patatero','21.30.40.50',53);
-    echo "<br><br><br>";
-    $PC02->insertDeviceIntoDatabase('admin');
-    foreach ($PC02->getDeviceProperties() as $fila){
-        echo $fila . "<br>";
-    }
